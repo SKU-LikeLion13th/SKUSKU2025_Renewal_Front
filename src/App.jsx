751bb76529b62c974824fa169e3e357a1bd69369
minseo121 <kittse1217@gmail.com>
2025-03-22 18:01:50 +0900

    기본 세팅

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './css/App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-center text-2xl'>
+        hello world!
+      </div>
+    </>
+  )
+}
+
+export default App
