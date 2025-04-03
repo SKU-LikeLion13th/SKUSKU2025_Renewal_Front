59ec7450bb81761233d8b396aa26bb8952aa2ce1
이호연 <ihoyeon@ihoyeon-ui-MacBookAir.local>
2025-04-03 17:29:51 +0900

    [fix] 메인 nav

---
+++ src/pages/User/Main/Main3.jsx
@@ -0,0 +1,231 @@
+export default function Main3() {
+  return (
+    <div>
+      {/* 멋사에서 진행되는 프로그램을 소개합니다 */}
+      <div className="bg-[#0E0E0E] text-white h-[300px] flex justify-center items-center text-[20px]">
+        <p className="fontThin">
+          멋쟁이사자처럼 13기에서 진행되는{" "}
+          <span className="fontBold">프로그램</span>을 소개합니다
+        </p>
+      </div>
+      {/* @2025 PROGRAM info  */}
+      <div className="bg-[#1B1B1B] py-20 pb-40 space-y-20">
+        <p className="text-[#9ABFFF] fontSB text-center">@2025 PROGRAM info </p>
+        <div className="max-w-4xl mx-auto space-y-40">
+          {/* 1 */}
+          <div className="grid grid-cols-2 gap-32 items-center text-white">
+            <img
+              src="/assets/images/Main/Main3_1.png"
+              className="rounded-[15px]"
+            />
+            <div className="flex flex-col p-4 space-y-6 ">
+              <p className="text-[30px] fontSB ">
+                함께 공부하는 <span className="text-[#72A6FF]">스터디</span>
+              </p>
+              <p className="text-[#E5E5E5] fontThin text-[15px]">
+                공부하고 싶은 트랙을 함께 공부하며
+                <br />
+                지식을 습득할 수 있는 학습의 장이 마련됩니다.
+              </p>
+              <div className="text-[13px]">
+                <div className="flex items-center">
+                  <img
+                    src="../../../public/assets/images/Main/place.png"
+                    alt=""
+                    className="w-2.5"
+                  />
+                  <p className="ml-2 text-[#E5E5E5] fontThin">
+                    성결대학교 성결관
+                  </p>
+                </div>
+                <div className="flex items-center mt-0.5">
+                  <img
+                    src="../../../public/assets/images/Main/date.png"
+                    alt=""
+                    className="w-3"
+                  />
+                  <p className="ml-2 text-[#E5E5E5] fontThin">
+                    스터디 별로 상이{" "}
+                    <span className="text-[12px]">
+                      (스쿠스쿠 사이버캠퍼스 내 일정 참고)
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* 2 */}
+          <div className="grid grid-cols-2 gap-32 items-center text-white">
+            <div className="flex flex-col p-4 space-y-6 ">
+              <p className="text-[30px] fontSB ">
+                세분화된 교육, <span className="text-[#72A6FF]">정기세션</span>
+              </p>
+              <p className="text-[#E5E5E5] fontThin text-[15px]">
+                아기사자들에게 트랙별로 교육을 제공합니다.
+                <br />
+                매주 정기세션에서 아기사자들과 운영진이 함께 성장합니다.
+              </p>
+              <div className="text-[13px]">
+                <div className="flex items-center ">
+                  <img
+                    src="../../../public/assets/images/Main/place.png"
+                    alt=""
+                    className="w-2.5"
+                  />
+                  <p className="ml-2 text-[#E5E5E5] fontThin">
+                    성결대학교 성결관
+                  </p>
+                </div>
+                <div className="flex items-center mt-0.5">
+                  <img
+                    src="../../../public/assets/images/Main/date.png"
+                    alt=""
+                    className="w-3"
+                  />
+                  <p className="ml-2 text-[#E5E5E5] fontThin ">
+                    매주 목요일 18시-21시
+                  </p>
+                </div>
+              </div>
+            </div>
+            <img
+              src="/assets/images/Main/Main3_2.png"
+              className="rounded-[15px]"
+            />
+          </div>
+
+          {/* 3 */}
+          <div className="grid grid-cols-2 gap-32 items-center text-white">
+            <img
+              src="/assets/images/Main/Main3_3.png"
+              className="rounded-[15px]"
+            />
+            <div className="flex flex-col p-4 space-y-6 ">
+              <p className="text-[30px] fontSB">
+                서비스의 초석 <span className="text-[#72A6FF]">아이디어톤</span>
+              </p>
+              <p className="text-[#E5E5E5] fontThin  text-[15px]">
+                공부하고 싶은 트랙을 함께 공부하며
+                <br />
+                지식을 습득할 수 있는 학습의 장이 마련됩니다.
+              </p>
+              <div className="text-[13px]">
+                <div className="flex items-center">
+                  <img
+                    src="../../../public/assets/images/Main/place.png"
+                    alt=""
+                    className="w-2.5"
+                  />
+                  <p className="ml-2 text-[#E5E5E5] fontThin">
+                    성결대학교 성결관
+                  </p>
+                </div>
+                <div className="flex items-center mt-0.5">
+                  <img
+                    src="../../../public/assets/images/Main/date.png"
+                    alt=""
+                    className="w-3"
+                  />
+                  <p className="ml-2 text-[#E5E5E5] fontThin">
+                    스터디 별로 상이{" "}
+                    <span className="text-[12px]">
+                      (스쿠스쿠 사이버캠퍼스 내 일정 참고)
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* 4 */}
+          <div className="grid grid-cols-2 gap-32 items-center text-white">
+            <div className="flex flex-col p-4 space-y-6 ">
+              <p className="text-[30px] fontSB ">
+                상상을 현실로 만드는{" "}
+                <span className="text-[#72A6FF]">해커톤</span>
+              </p>
+              <p className="text-[#E5E5E5] fontThin text-[15px]">
+                공부하고 싶은 트랙을 함께 공부하며
+                <br />
+                지식을 습득할 수 있는 학습의 장이 마련됩니다.
+              </p>
+              <div className="text-[13px]">
+                <div className="flex items-center">
+                  <img
+                    src="../../../public/assets/images/Main/place.png"
+                    alt=""
+                    className="w-2.5"
+                  />
+                  <p className="ml-2 text-[#E5E5E5] fontThin">
+                    성결대학교 성결관
+                  </p>
+                </div>
+                <div className="flex items-center mt-0.5">
+                  <img
+                    src="../../../public/assets/images/Main/date.png"
+                    alt=""
+                    className="w-3"
+                  />
+                  <p className="ml-2 text-[#E5E5E5] fontThin">
+                    스터디 별로 상이{" "}
+                    <span className="text-[12px]">
+                      (스쿠스쿠 사이버캠퍼스 내 일정 참고)
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <img
+              src="/assets/images/Main/Main3_4.png"
+              className="rounded-[15px]"
+            />
+          </div>
+
+          {/* 5 */}
+          <div className="grid grid-cols-2 gap-32 items-center text-white">
+            <img
+              src="/assets/images/Main/Main3_5.png"
+              className="rounded-[15px]"
+            />
+            <div className="flex flex-col p-4 space-y-6 ">
+              <p className="text-[30px] fontSB ">
+                하계 <span className="text-[#72A6FF]">MT</span>
+              </p>
+              <p className="text-[#E5E5E5] fontThin text-[15px]">
+                공부하고 싶은 트랙을 함께 공부하며
+                <br />
+                지식을 습득할 수 있는 학습의 장이 마련됩니다.
+              </p>
+              <div className="text-[13px]">
+                <div className="flex items-center">
+                  <img
+                    src="../../../public/assets/images/Main/place.png"
+                    alt=""
+                    className="w-2.5"
+                  />
+                  <p className="ml-2 text-[#E5E5E5] fontThin">
+                    성결대학교 성결관
+                  </p>
+                </div>
+                <div className="flex items-center mt-0.5">
+                  <img
+                    src="../../../public/assets/images/Main/date.png"
+                    alt=""
+                    className="w-3"
+                  />
+                  <p className="ml-2 text-[#E5E5E5] fontThin">
+                    스터디 별로 상이{" "}
+                    <span className="text-[12px]">
+                      (스쿠스쿠 사이버캠퍼스 내 일정 참고)
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
