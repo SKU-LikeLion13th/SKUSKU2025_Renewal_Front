b0e7c15818ce31d49680775a423fce748d513ecd
이호연 <ihoyeon@ihoyeon-ui-MacBookAir.local>
2025-04-21 21:56:31 +0900

    [add]구글 로그인, 강의자료, 관리자  깔짝

---
+++ src/components/GoogleLoginBtn.jsx
@@ -0,0 +1,25 @@
+export default function GoogleLoginBtn({ isHovered }) {
+  const handleLogin = () => {
+    const currentPath = location.pathname + location.search;
+    localStorage.setItem("redirectAfterLogin", currentPath);
+    window.location.href =
+      "http://backend.sku-sku.com/oauth2/authorization/google";
+  };
+
+  return (
+    <div
+      className={`flex items-center px-4 py-2 rounded-sm cursor-pointer
+          ${isHovered ? "bg-white text-black border" : "bg-white/50 text-white"}
+          hover:bg-white  hover:text-black transition`}
+      onClick={handleLogin}
+    >
+      <img
+        src="/assets/images/googleLogin.png"
+        alt=""
+        width="16px"
+        className="mr-2"
+      />
+      <p className="text-[13px]">구글계정으로 계속하기</p>
+    </div>
+  );
+}
