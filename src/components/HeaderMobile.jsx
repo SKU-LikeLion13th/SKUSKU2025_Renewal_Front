b56648e4f414fecdca13c9ad1c16cff409342f93
이호연 <ihoyeon@ihoyeon-ui-MacBookAir.local>
2025-05-22 19:47:54 +0900

    [fix]반응형

--- src/components/HeaderMobile.jsx
+++ src/components/HeaderMobile.jsx
@@ -9,7 +9,7 @@ export default function HeaderMobile() {
   return (
     <div
       className={`fixed z-10 bg-[#121212] top-0 w-full transition-all duration-300 ${
-        isHovered || isMenuOpen ? "bg-white min-h-[280px]" : "backdrop-blur-2xl"
+        isHovered || isMenuOpen ? "bg-white min-h-[180px]" : "backdrop-blur-2xl"
       }`}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => {
@@ -70,101 +70,6 @@ export default function HeaderMobile() {
         )}
 
         {/* 메뉴 리스트 */}
-        <div
-          className={`${
-            isMenuOpen ? "flex" : "hidden"
-          } flex-col mt-4 space-y-4 md:flex md:flex-row md:space-y-0 md:space-x-16 md:mt-0`}
-        >
-          {["PROJECT", "TEAM", "COMMUNITY"].map((title, index) => (
-            <div
-              key={index}
-              className={`fontMedium text-sm md:text-[16px] ${
-                isHovered || isMenuOpen ? "text-[#000]" : "text-white"
-              } cursor-pointer relative`}
-            >
-              {title}
-              {(isHovered || isMenuOpen) && (
-                <div className="absolute w-full space-y-6 mt-12">
-                  {title === "COMMUNITY" ? (
-                    <div className="text-sm text-[#121212] text-center fontRegular">
-                      문의사항
-                    </div>
-                  ) : (
-                    <>
-                      <div className="text-sm text-[#121212] text-center fontRegular">
-                        13기
-                      </div>
-                      <div className="text-sm text-[#121212] text-center fontRegular">
-                        12기
-                      </div>
-                      <div className="text-sm text-[#121212] text-center fontRegular">
-                        11기
-                      </div>
-                    </>
-                  )}
-                </div>
-              )}
-            </div>
-          ))}
-        </div>
-
-        {/* 오른쪽 버튼들 */}
-        <div className="hidden md:flex items-center space-x-4 mt-4 md:mt-0">
-          <div
-            className={`transition-opacity duration-300 space-x-4 ${
-              isHovered || isMenuOpen ? "flex opacity-100" : "hidden opacity-0"
-            }`}
-          >
-            <button className="text-[13px] px-6 py-1 flex items-center rounded-3xl text-[#fff] bg-[#212121] ">
-              제작자
-            </button>
-            <a
-              href="https://www.instagram.com/likelion_sku"
-              className="flex items-center"
-            >
-              <img
-                src="/assets/images/insta.png"
-                className="w-7"
-                alt="Instagram"
-              />
-            </a>
-            <a href="#" className="flex items-center mr-6">
-              <img src="/assets/images/chat.png" className="w-7" alt="Chat" />
-            </a>
-          </div>
-
-          <div
-            className={`flex text-white items-center px-4 py-2 rounded-sm ${
-              isHovered || isMenuOpen
-                ? "bg-[#8DB7FF] hover:bg-[#2D5ABB]"
-                : "bg-[#2D5ABB]"
-            }`}
-          >
-            <img
-              src="/assets/images/campus.png"
-              alt="Campus Icon"
-              width={"16px"}
-              className="mr-2"
-            />
-            <p className="text-[14px]">CYBERCAMPUS</p>
-          </div>
-
-          <div
-            className={`flex items-center px-4 py-2 rounded-sm ${
-              isHovered || isMenuOpen
-                ? "border border-[#000] text-[#000] bg-white"
-                : "bg-white/50 text-white"
-            }`}
-          >
-            <img
-              src="/assets/images/googleLogin.png"
-              alt="Google Icon"
-              width={"16px"}
-              className="mr-2"
-            />
-            <p className="text-[13px]">구글계정으로 계속하기</p>
-          </div>
-        </div>
       </div>
     </div>
   );
--- src/pages/User/Main/Main.jsx
+++ src/pages/User/Main/Main.jsx
@@ -3,11 +3,15 @@ import Main1 from "./Main1";
 import Main2 from "./Main2";
 import Main3 from "./Main3";
 import Main4 from "./Main4";
+import MobileMain4 from "./MobileMain4";
 import Main5 from "./Main5";
+import { useMediaQuery } from "./useMediaQuery";
 
 export default function Main() {
+  const isMobile = useMediaQuery("(max-width: 640px)");
+
   return (
-    <div className="w-full bg-black mt-15">
+    <div className="w-full bg-black sm:mt-0 mt-15">
       <div className="section">
         <Main1 />
       </div>
@@ -17,9 +21,10 @@ export default function Main() {
       <div className="section">
         <Main3 />
       </div>
-      <div className="section">
+      <div className="section">{isMobile ? <MobileMain4 /> : <Main4 />}</div>
+      {/* <div className="section">
         <Main4 />
-      </div>
+      </div> */}
       <div className="section">
         <Main5 />
       </div>
