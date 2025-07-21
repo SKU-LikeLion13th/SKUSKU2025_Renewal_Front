e81f1de8543b8590821a2fea1355383b2e04c2a7
이호연 <ihoyeon@ihoyeon-ui-MacBookAir.local>
2025-07-21 11:22:14 +0900

    머였지 ,,

--- src/pages/User/Main/Main5.jsx
+++ src/pages/User/Main/Main5.jsx
@@ -8,7 +8,7 @@ export default function Main5() {
         <div>
           <div className="flex flex-col text-center justify-center">
             <p className="text-[#3B79FF] fontBold text-[30px]">PROJECTS</p>
-            <p className="fontThin text-[#ffffff] mt-8">
+            <p className="fontThin text-[#ffffff] my-8">
               성결대학교 멋쟁이사자처럼과 함께한 프로젝트들을 소개합니다.
             </p>
           </div>
