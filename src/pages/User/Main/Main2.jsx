77ae9c468db6a8b9b0136e40df9f031825013fae
이호연 <ihoyeon@ihoyeon-ui-MacBookAir.local>
2025-08-01 19:21:15 +0900

    [fix]Main1,Main2

--- src/pages/User/Main/Main2.jsx
+++ src/pages/User/Main/Main2.jsx
@@ -21,12 +21,15 @@ function AnimatedCard({ title, desc, imgSrc, imgClass, bgColor }) {
       variants={fadeUp}
       initial="hidden"
       animate={isInView ? "visible" : "hidden"}
-      className="flex flex-1 min-w-0 sm:max-w-[350px] max-w-[33%] h-auto sm:h-48 aspect-[4/3] sm:aspect-auto rounded-sm sm:rounded-xl items-center justify-between px-2 sm:px-8 py-2"
+      className="flex flex-1 min-w-0 sm:max-w-[26%] w-full h-auto sm:h-48 aspect-[4/3] sm:aspect-auto rounded-sm sm:rounded-xl items-center justify-evenly px-2 sm:px-3 py-2"
       style={{ backgroundColor: bgColor }}
     >
       <div className="flex flex-col justify-center gap-1 sm:gap-2 text-left overflow-hidden">
-        <p className="fontSB text-[11px] sm:text-[clamp(11px,2.9vw,30px)] truncate">{title}</p>
-        <div className="hidden sm:block fontEL text-sm break-words leading-5">
+        <p className="fontSB text-[11px] sm:text-[clamp(11px,2.9vw,26px)] truncate">
+          {title}
+        </p>
+        {/* <div className="hidden lg:block fontEL text-sm break-words leading-5"> */}
+        <div className="hidden lg:block fontEL leading-5 break-words text-[clamp(8px,2vw,15px)]">
           {desc}
         </div>
       </div>
@@ -47,7 +50,8 @@ export default function Main2() {
         </>
       ),
       imgSrc: "/assets/images/Main/Self.png",
-      imgClass: "sm:w-15 w-6",
+      // imgClass: "lg:w-15 md:w-13 sm:w-10 w-6",
+      imgClass: "w-[clamp(24px,5vw,60px)]",
       bgColor: "#4B76D3",
     },
     {
@@ -62,7 +66,8 @@ export default function Main2() {
         </>
       ),
       imgSrc: "/assets/images/Main/cooperation.png",
-      imgClass: "sm:w-26 w-8",
+      // imgClass: "lg:w-26 md:w-20 sm:w-12 w-8",
+      imgClass: "w-[clamp(40px,7vw,90px)]",
       bgColor: "#264C9F",
     },
     {
@@ -75,7 +80,8 @@ export default function Main2() {
         </>
       ),
       imgSrc: "/assets/images/Main/Possibility.png",
-      imgClass: "sm:w-18 w-6",
+      // imgClass: "lg:w-18 md:w-14 sm:w-10 w-6",
+      imgClass: "w-[clamp(24px,5vw,60px)]",
       bgColor: "#142F69",
     },
   ];
@@ -102,7 +108,7 @@ export default function Main2() {
           "내 아이디어를 내 손으로 실현한다."
         </p>
         {/* 박스 3개 */}
-        <div className="flex w-full px-4 sm:px-0 justify-center gap-2 sm:gap-20 mt-2">
+        <div className="flex flex-wrap w-full px-4 sm:px-0 justify-center gap-x-2 gap-y-4 sm:gap-x-6 sm:gap-y-6 mt-2">
           {items.map(({ title, desc, imgSrc, imgClass, bgColor }, idx) => (
             <AnimatedCard
               key={idx}
@@ -118,10 +124,14 @@ export default function Main2() {
         {/* 글 */}
         <div className="text-center hidden sm:block leading-8">
           <p className="fontThin sm:text-[clamp(9px,2.9vw,16px)] ">
-            <span className="fontSB text-xl">"내 아이디어를 내 손으로 실현한다." </span>
+            <span className="fontSB text-xl">
+              "내 아이디어를 내 손으로 실현한다."{" "}
+            </span>
             라는 모토를 가지고,
           </p>
-          <p className="fontThin">실제 서비스를 구현하며 개발자의 꿈을 이루는데</p>
+          <p className="fontThin">
+            실제 서비스를 구현하며 개발자의 꿈을 이루는데
+          </p>
           <p className="fontThin">한걸음 더 다가가고자 합니다.</p>
         </div>
       </div>
